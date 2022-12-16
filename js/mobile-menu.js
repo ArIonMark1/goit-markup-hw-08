7fa62b41e4573dc359666f17f8e790c9999f5762
ArIonMark1 <andreydatsenko777@gmail.com>
2022-12-16 13:09:11 +0100

    bugfix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,21 +1,50 @@
 
-let modal = document.getElementsByClassName(".js-menu-container");
-let openButton = document.getElementsByClassName(".js-open-menu");
-let closeButton = document.getElementsByClassName(".js-close-menu");
-(
-    () => { 
-        const refs = {
-            openButton: document.getElementsByClassName(".js-open-menu"),
-            closeButton: document.getElementsByClassName(".js-close-menu"),
-            modal: document.getElementsByClassName(".js-menu-container"),
-        };
+// let modal = document.getElementsByClassName(".js-menu-container");
+// let openButton = document.getElementsByClassName(".js-open-menu");
+// let closeButton = document.getElementsByClassName(".js-close-menu");
+// (
+//     () => { 
+//         const refs = {
+//             openButton: document.getElementsByClassName(".js-open-menu"),
+//             closeButton: document.getElementsByClassName(".js-close-menu"),
+//             modal: document.getElementsByClassName(".js-menu-container"),
+//         };
 
-        refs.openButton.addEventListener("click", toggleMenu);
-        refs.closeButton.addEventListener("click", toggleMenu);
+//         refs.openButton.addEventListener("click", toggleMenu);
+//         refs.closeButton.addEventListener("click", toggleMenu);
 
-        function toggleMenu() { 
-            refs.modal.classList.toggle('is-open');
-            document.body.style.overflow = 'auto'; 
-        }
-    }
-)
+//         function toggleMenu() { 
+//             refs.modal.classList.toggle('is-open');
+//             document.body.style.overflow = 'auto'; 
+//         }
+//     }
+// )
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
