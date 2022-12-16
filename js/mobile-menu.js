3bcc077bb8d42f1f64b9a757ae4bba06254a2069
ArIonMark1 <andreydatsenko777@gmail.com>
2022-12-16 12:30:40 +0100

    modal navigation

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,43 +1,21 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+let modal = document.getElementsByClassName(".js-menu-container");
+let openButton = document.getElementsByClassName(".js-open-menu");
+let closeButton = document.getElementsByClassName(".js-close-menu");
+(
+    () => { 
+        const refs = {
+            openButton: document.getElementsByClassName(".js-open-menu"),
+            closeButton: document.getElementsByClassName(".js-close-menu"),
+            modal: document.getElementsByClassName(".js-menu-container"),
+        };
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
+        refs.openButton.addEventListener("click", toggleMenu);
+        refs.closeButton.addEventListener("click", toggleMenu);
 
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+        function toggleMenu() { 
+            refs.modal.classList.toggle('is-open');
+            document.body.style.overflow = 'auto'; 
+        }
+    }
+)
